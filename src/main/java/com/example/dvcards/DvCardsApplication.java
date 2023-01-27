2badd5b540faa85a8b765b95258a88e8be352db9
SpicyW0lf <62665736+SpicyW0lf@users.noreply.github.com>
2023-01-27 19:18:21 +0300

    Initialised

---
+++ src/main/java/com/example/dvcards/DvCardsApplication.java
@@ -0,0 +1,13 @@
+package com.example.dvcards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvCardsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DvCardsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dvcards/DvCardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dvcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvCardsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
