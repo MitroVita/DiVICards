6006b6b7d7fce043cfb519b9ba980f393eca1493
SpicyW0lf <62665736+SpicyW0lf@users.noreply.github.com>
2023-01-27 20:49:39 +0300

    Revert "First migration(not working)"
    This reverts commit d6fec7501bb6427639d6fa80fb86852c84cdedb7.

--- db/migration/V0_1_0__create_table.sql
+++
@@ -1,5 +0,0 @@
-create table if not exists "Users" (
-    "Id" varchar(30) not null auto_increment primary key,
-    "Login" varchar(20) not null,
-    "Password" varchar(20) not null
-)ENGINE=InnoDB default CHARSET=UTF8
